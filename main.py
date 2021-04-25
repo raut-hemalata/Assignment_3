9ba2b8acafa0e674b70aaac34e81f74d9099dd34
Hemalata Raut <80773589+raut-hemalata@users.noreply.github.com>
2021-04-26 00:43:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pendulum
 import socket
 from geopy.geocoders import Nominatim
+from flask import Flask, render_template,url_for
+app = Flask(__name__)
 ist = pendulum.timezone('Asia/Calcutta')
+Time=pendulum.now(ist)
 hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+ip_address = socket.gethostbyname(hostname)
+print("Current Time in IST : ",Time)
+print(f"IP Address: {ip_address}")
+print(f"Hostname: {hostname}")
 geolocator = Nominatim(user_agent="geoapiExercises")
 Latitude = "18.5204"
 Longitude = "73.8567"
@@ -12,9 +18,17 @@ address = location.raw['address']
 city = address.get('city', '')
 state = address.get('state', '')
 country = address.get('country', '')
-print("Current Time in IST : ",pendulum.now(ist))
-print("Your Computer Name is:" + hostname)
-print("Your Computer IP Address is:" + IPAddr)
 print("City :" ,city)
 print("Region :",state)
 print("Country :",country)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/pucsd")
+def pucsd():
+    return render_template("pucsd.html",content1=Time,content2=hostname,content3=ip_address,content4=city,content5=state,content6=country)
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0')
+    app.run(debug=True)
+
